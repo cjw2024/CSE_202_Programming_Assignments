9a8df6a282ca903197b81ab0b76ceb0a59a613ab
Caden Wright <cjw224@lehigh.edu>
2023-01-30 16:56:17 -0500

    cleaned code and added documention

--- p0/prog0.c
+++ p0/prog0.c
@@ -5,63 +5,58 @@
 
 #include "user.h"
 
+// global variables to save to and use
 user_t* user_list;
 char* filename = "users.txt";
 int user_count;
 int change = 0;
 
-int main(int argc, char** argv){
+/**
+ * @brief user interface
+ * 
+ * @return int 
+ */
+int main(){
     int lines = 1;
     char c;
-
+    //open file to read it
     FILE *file = fopen(filename, "r");
-
+    //count how many lines are in the file
     for(c = getc(file); c != EOF; c = getc(file)){
         if(c == '\n'){
             lines++;
         }
     }
 
-    fclose(file);
+    fclose(file);//close the file
 
-    //printf("%d\n", lines);
-
+    //create memory space for the user_list with the amount of lines in the file
     user_list = (user_t *)malloc(lines*sizeof(user_t));
-    user_count = read_users(user_list, filename);
+    user_count = read_users(user_list, filename);   //read the file to the user_list
     if(user_count != -1){
         printf("File \"users.txt\" loaded successfully.\n");
     } else {
         printf("File \"users.txt\" loaded unsuccessfully.\n");
         return 0;
     }
-    //printf("%d\n", user_count);
-
-    /*for(int i = 0; i < user_count; i++){
-        printf("%c\n", user_list[i].val);
-    }*/
 
     int index;
-
+    //3 attempts to log in
     for(int i = 0; i < 3; i++){
-        //puts(user_list[11].password);
         printf("Enter login credentials:\n");
-
+        //user input for username
         printf("username: ");
         char username[100];
         scanf("%99[^\n]", username);
         getchar();
-
+        //user input for passwors
         char password[100];
         printf("password: ");
         scanf("%99[^\n]", password);
         getchar();
 
-        //break;
-        //puts(username);
-        //puts(password);
-
-        index = find_user(user_list, username, password, user_count);
-        //printf("%d\n", index);
+        index = find_user(user_list, username, password, user_count);   //search to see if match
+        //if match break the for loop
         if(index != -1)
             break;
         else{
@@ -74,92 +69,86 @@ int main(int argc, char** argv){
         }
 
     }
-
-    //printf("%d\n", index);
-
+    //if user does not have proper priveledges
     if(user_list[index].val == 48){
         printf("You do not have administrator access rights.  You cannot access the file. \n\n");
         return 0;
     }
 
     int loop_needed = -1;
-
+    //loop through the menu for user interaction
     while(loop_needed == -1){
         printf("\n");
         printf("Select an operation:\n");
         printf("1: Add a new user\n");
         printf("2: Reset Password of an existing user\n");
         printf("3: Logout\n");
-
+        //take user input for menu option
         int option = 0;
         scanf("%d", &option);
         getchar();
-        //printf("%d\n", option);
-
+        //adding new user
         if(option == 1){
+            //take user input for new username
             printf("username: ");
             char username[100];
             scanf("%99[^\n]", username);
             getchar();
-
+            //take user input for their priviledges
             char user_privilege;
             printf("Enter user privileges (1 for admin, 0 for user): ");
             scanf("%c", &user_privilege);
             getchar();
-
+            //if valid input(1 or 0)
             if(user_privilege == 48 | user_privilege == 49){
-                
+                //create new password
                 char* pass;
                 new_password(&pass);
                 user_t new_user;
+                //create memory for the username and password to be saved
                 new_user.username = malloc(strlen(username) + 1);
                 new_user.password = malloc(strlen(pass) + 1);
-                
+                //copy to the user_t struct
                 strcpy(new_user.username, username);
                 strcpy(new_user.password, pass);
                 new_user.val = user_privilege;
-
+                //increase # of users and reallocate memory for a bigger list
                 user_count++;
                 user_list = (user_t*)realloc(user_list, user_count * sizeof(user_t));
                 if(user_list == NULL){
                     printf("error");
                     return -1;
                 }
-
+                //save new user
                 user_list[user_count - 1] = new_user;
-                printf("%s\n", user_list[user_count-2].username);
-                //printf("%s\n", user_list[user_count-1].password);
-                //printf("%d\n", user_list[user_count-1].val);
 
                 change = 1;
                 printf("User added successfully: ");
                 puts(username);
 
-            } else 
+            } else //invalid input
                 printf("User unsuccessfully added\n\n");
-            
+        //resetting password    
         }else if(option == 2){
+            //take user input for user resetting password
             printf("Enter username: ");
             char username[100];
             scanf("%99[^\n]", username);
             getchar();
-
-            //int found = 0;
+            //search for username
             int found = find_username(user_list, username, user_count);
-            if(found != -1){
-                //puts(user_list[found].password);
+            if(found != -1){    //change password
                 reset_password(&user_list[found]);
                 printf("password reset successfully.\n\n");
                 change = 1;
-            }else
+            }else   //not found
                 printf("Unable to find username.\n\n");
-            //puts(user_list[found].password);
             
-
+        //logging out and updating file
         }else if(option == 3){
-            loop_needed = 0;
+            loop_needed = 0;    //exit the while loop
             printf("Updating the data file ...\n");
-
+            // if the user_list was changed in any way
             if(change == 1){
                 int saved = save_users(user_list, filename, user_count);
                 if(saved != 0)
@@ -167,13 +156,12 @@ int main(int argc, char** argv){
             }
             
             printf("logout complete\n\n");
+        //invalid menu input
         }else{
-
+            printf("Unvalid input. Try again.\n\n");
         }
 
     }
-
-
 
 }
 
--- p0/user.c
+++ p0/user.c
@@ -6,171 +6,150 @@
 
 #include "user.h"
 
-/*char* filename = "users.txt";
-user_t* user_list;
-user_t user;
-
-int main(int argc, char** argv){
-    user.password = (char*)malloc(sizeof(char));
-    user.password = "no";
-    puts(user.password);
-    reset_password(&user);
-    //new_password(argv[1]);
-    puts(user.password);
-
-    //char* filename = "users.txt";
-    user_list = (user_t *)malloc(100*sizeof(user_t));
-    int user_count = read_users(user_list, filename);
-    //printf("%d\n", user_count);
-    puts(user_list[0].username);
-    puts(user_list[0].password);
-
-    for (int i = 0; i < 100; i++){
-        printf("%s\n", user_list[i].username);
-        printf("%s\n", user_list[i].password);
-        //printf("%d\n", user_list[i].val);
-        printf("%c\n\n", user_list[i].val);
-    }
-
-    int saved = save_users(user_list, filename, user_count);
-    printf("%d\n", saved);
-
-    char* name = "mar210";
-    int index = find_username(user_list, name, user_count);
-    printf("%d\n", index);
-
-    char* name2 = "ama220";
-    char* pass2 = "m]Ig[t&AX:";
-    int index2 = find_user(user_list, name2, pass2, user_count);
-    printf("%d\n", index2);
-
-    char* name3 = "lca213";
-    char* pass3 = "`akFzM?_6";
-    int index3 = find_user(user_list, name3, pass3, user_count);
-    printf("%d\n", index3);
-
-}*/
-
+/**
+ * @brief creates a new password
+ * 
+ * @param pass location of string pass to save new password
+ */
 void new_password(char **pass){
-    /**pass = NULL;
+    //random length from 8-10
     int length = (rand() % 3) + 8;
-    //*pass = (char*)malloc(length+1);  
-    //puts(pass);  
-    int i = 0;
-    
-    char* new[10];
-    int n = 94;
-    srand(time(0));
-
-    for(; i < length; i++){
-        new[i] = (char) (rand() % n + 32);
-    }
-    new[i] = '\0';
-
-    //*pass = new;
-    //puts(*pass);
-    strcpy(*pass, new);*/
-
-    int length = (rand() % 3) + 8;
+    //free the memory for pass
     *pass = (char*)malloc(length+1);
     int i = 0;
-    int n = 93;
+    int n = 93; //how many valid ascii values there are
     srand(time(0));
 
+    //add randome ascii value
     for(; i < length; i++){
-        *(*pass + i) = (char) (rand() % n + 33);
+        *(*pass + i) = (char) (rand() % n + 33);    // +33 goes to first valid ascii
     }
 
+    //end the new password
     *(*pass + i) = '\0';
 };
 
+/**
+ * @brief resets the password of a user_t
+ * 
+ * @param u the user who is resetting password
+ */
 void reset_password(user_t *u){
     new_password(&u->password);
 };
 
+/**
+ * @brief read users from a given file
+ * 
+ * @param user_list the list of users to save to
+ * @param filename the name of the file reading from
+ * @return int, the number of users read
+ */
 int read_users(user_t *user_list, char* filename){
-
+    //Open the file to read from it
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         return -1;
     }
 
+    //Create memory for the buffer to read from the file
     char **buffer = (char **)malloc(sizeof(char*));
     int n = 0;
     size_t size;
 
+    //save to the buffer and reallocate the memory so it saves properly
     while(getline(&buffer[n], &size, file) != -1){
         n++;
         buffer = (char **)realloc(buffer, (n+1) * sizeof(char*));
     }
 
-    fclose(file);
+    fclose(file);   //close the file
 
+    //iterate throw the array buffer to save to user_list
     for(int i = 0; i < n; i++){
-        //printf("%s", buffer[i]);
+        // break apart each line into user_t values
         char* user = strtok(buffer[i], " ");
         char* pass = strtok(NULL, " ");
         char* num = strtok(NULL, " ");
 
+        //free memory for the username and password to be saved
         user_list[i].username = malloc(strlen(user) + 1);
         user_list[i].password = malloc(strlen(pass) + 1);
-        //user_list[i].val = malloc(sizeof(char));
-        
+        //copy over username and password
         strcpy(user_list[i].username, user);
         strcpy(user_list[i].password, pass);
-        //strcpy(user_list[i].val, num[0]);
-        user_list[i].val = (int) num[0];
-
-        //printf("%s\n", user_list[i].username);
-        //printf("%s\n", user_list[i].password);
-        //printf("%d\n", user_list[i].val);
-        //printf("%c\n\n", user_list[i].val);
+        user_list[i].val = (int) num[0];    //save the priviledge
     }
     
     return n;
 };
 
+/**
+ * @brief Saves users from the user_list to a file
+ * 
+ * @param user_list the list of user_t
+ * @param filename the list of the file saving to
+ * @param size the amount of user_t
+ * @return int, 0 if saved properly
+ */
 int save_users(user_t *user_list, char* filename, int size){
-
+    //open file to write to it
     FILE* file = fopen(filename, "w");
     if (file == NULL) {
         return -1;
     }
 
+    //iterate throw user_t and print to the file
     int i = 0;
     for(; i < size-1; i++){
         fprintf(file, "%s %s %c\n", user_list[i].username, user_list[i].password, user_list[i].val);
     }
     fprintf(file, "%s %s %c", user_list[i].username, user_list[i].password, user_list[i].val);
 
-    fclose(file);
+    fclose(file);   //close the file
     return 0;
 };
 
+/**
+ * @brief search for a username in the user_list
+ * 
+ * @param user_list list of user_t
+ * @param username the username searching for
+ * @param size the amount of user_t in user_list
+ * @return int, index of the username if found
+ */
 int find_username(user_t *user_list, char* username, int size){
-    //puts(username);
-    //printf("%ld\n", strlen(username));
-    //printf("%ld\n", strlen("ama220"));
+    //iterate through the user list to find the username
     for(int i = 0; i < size; i++){
         if(strcmp(username, user_list[i].username) == 0){
             return i;
         }
     }
 
-    return -1;
+    return -1;  //not found
 };
 
+/**
+ * @brief find a user by searching for username and password
+ * 
+ * @param user_list list of user_t
+ * @param username the username searching for
+ * @param password the password comparing to match
+ * @param size the amount of user_t in user_list
+ * @return int the index of the user
+ */
 int find_user(user_t *user_list, char* username, char* password, int size){
-
+    //search for the username
     int user_index = find_username(user_list, username, size);
     
+    //username found
     if(user_index != -1){
         if(strcmp(user_list[user_index].password, password) == 0){
-            return user_index;
+            return user_index;  //password match
         } else {
-            return -1;
+            return -1;  //password does not match
         }
     } 
 
-    return -1;
+    return -1;  //not found
 };
